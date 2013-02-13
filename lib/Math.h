529878d1ee8166b7ad746132479af691ecf4489e
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-12 20:41:15 -0500

    formatted Math.h

--- lib/Math.h
+++ lib/Math.h
@@ -1,9 +1,12 @@
 #ifndef MATH_H
 #define MATH_H
+
 #define PI 3.14159265358979323846
 #include <cstdlib>  // For srand() and rand()
 #include <math.h>
-double randn_notrig(double mu=0.0, double sigma=1.0);
-double randn_trig(double mu=0.0, double sigma=1.0);
+
+double randn_notrig(double mu = 0.0, double sigma = 1.0);
+double randn_trig(double mu = 0.0, double sigma = 1.0);
 double mapValues(double, double, double, double, double);
+
 #endif // MATH_H
