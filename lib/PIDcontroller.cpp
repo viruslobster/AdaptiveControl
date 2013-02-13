7569638cef49e41fb791405d0b43c0f035c9d463
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-12 20:45:14 -0500

    cleaned and formatted PIDcontroller.cpp

--- lib/PIDcontroller.cpp
+++ lib/PIDcontroller.cpp
@@ -10,82 +10,75 @@ using namespace std;
 using namespace boost;
 
 PIDcontroller::PIDcontroller() {
-  isfirstrun = true;
-  Kp = 0;
-  Ki = 0;
-  Kd = 0;
-  min = 0;
-  max = 0;
-  
-
+    isfirstrun = true;
+    Kp = 0;
+    Ki = 0;
+    Kd = 0;
+    min = 0;
+    max = 0;
 }
 
 PIDcontroller::PIDcontroller(double min, double max) {
-  this->isfirstrun = true;
-  this->Kp = 1.2;
-  this->Ki = 0.001;
-  this->Kd = 30;
-  this->min = min;
-  this->max = max;
-  
+    this->isfirstrun = true;
+    this->Kp = 1.2;
+    this->Ki = 0.001;
+    this->Kd = 30;
+    this->min = min;
+    this->max = max;
+
 
 }
 
 double PIDcontroller::getCorrection (double error) {
-  //double time = getTimeMilli();
-  //double d_error = error - lastError;
-  //double d_time  = time - lastTime;
-  double correction;
-  if(isfirstrun) {    
-    correction = Kp * error;
-    lastError = error;
-    isfirstrun = false;
-  } else {
-    double d_error = error - lastError;
-    integral += error;
-    correction = Kp * error + Ki * integral + Kd * d_error;
-    lastError = error;
-    
-  }
-  if(min != max) {
-   if(correction > max) correction = max;
-   if(correction < min) correction = min;
-  }
-  
-  return correction;
+    double correction;
+
+    if(isfirstrun) {
+        correction = Kp * error;
+        lastError = error;
+        isfirstrun = false;
+    } else {
+        double d_error = error - lastError;
+        integral += error;
+        correction = Kp * error + Ki * integral + Kd * d_error;
+        lastError = error;
+    }
+
+    if(min != max) {
+        if(correction > max) correction = max;
+
+        if(correction < min) correction = min;
+    }
+
+    return correction;
 }
 
 void PIDcontroller::loadConfig (char c) {
-  string filename = "pid.";
-  filename += c;
-  filename.append(".config");
-  ifstream file(filename.c_str());
-  
-  if(file.is_open()) {
-    string line;
-    getline(file, line);    
-    file.close();
-    
-    vector<string> fields;
-    split(fields, line, is_any_of(","));
-    double p, i, d;
-    p = atof(fields[0].c_str());
-    i = atof(fields[1].c_str());
-    d = atof(fields[2].c_str());
-    cout << "tune: " << p << ", " << i << ", " << d << endl;
-    tunePID(p, i, d);
-  } else {
-    cerr << "Cannot open " << filename << endl;
-  }
-  
+    string filename = "pid.";
+    filename += c;
+    filename.append(".config");
+    ifstream file(filename.c_str());
 
+    if(file.is_open()) {
+        string line;
+        getline(file, line);
+        file.close();
+
+        vector<string> fields;
+        split(fields, line, is_any_of(","));
+        double p, i, d;
+        p = atof(fields[0].c_str());
+        i = atof(fields[1].c_str());
+        d = atof(fields[2].c_str());        
+        tunePID(p, i, d);
+    } else {
+        cerr << "Cannot open " << filename << endl;
+    }
 }
 
-
 void PIDcontroller::tunePID(double Kp, double Ki, double Kd) {
-  this->Kp = Kp;
-  this->Ki = Ki;
-  this->Kd = Kd;
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
 }
 
 
