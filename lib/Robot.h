4ecd792e3ceef2c57a15e5351505447274d2f644
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-12 20:47:28 -0500

    formatted Robot.h

--- lib/Robot.h
+++ lib/Robot.h
@@ -2,10 +2,10 @@
 #define ROBOT_H
 
 struct Robot {
-  double x, y, theta;
-  void move(double distance, double turn);
-  Robot();
-  Robot(double x, double y);
+    double x, y, theta;
+    void move(double distance, double turn);
+    Robot();
+    Robot(double x, double y);
 };
 
 #endif // ROBOT_H
