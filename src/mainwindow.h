ec77731be63cef728c311dcd832b387c9d79d315
Michael Stevens <mls0.5mv2@gmail.com>
2013-02-12 20:53:45 -0500

    formatted mainwindow.h

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -8,37 +8,36 @@
 #include "Robot.h"
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-  Q_OBJECT
-    
-public:
-  explicit MainWindow(QWidget *parent = 0);
-  ~MainWindow();
-  double f(double x);
-  double g(double x);
-  double h(double x);
-  char pathFunction;
-  Robot* robot;
+class MainWindow : public QMainWindow {
+        Q_OBJECT
 
-private slots:
-  void updateLoop();
-  void on_radioButton_4_clicked();
-  void on_radioButton_3_clicked();
-  void on_radioButton_5_clicked();
-  void on_pushButton_2_clicked();
-  void on_pushButton_clicked();
+    public:
+        explicit MainWindow(QWidget *parent = 0);
+        ~MainWindow();
+        double f(double x);
+        double g(double x);
+        double h(double x);
+        char pathFunction;
+        Robot* robot;
 
-private:  
-  Ui::MainWindow *ui;
-  QTimer* updater;
-  Controller* controller;  
-  double speed;
-  double sum, count, lastAve;
-  std::string filename;
+    private slots:
+        void updateLoop();
+        void on_radioButton_4_clicked();
+        void on_radioButton_3_clicked();
+        void on_radioButton_5_clicked();
+        void on_pushButton_2_clicked();
+        void on_pushButton_clicked();
+
+    private:
+        Ui::MainWindow *ui;
+        QTimer* updater;
+        Controller* controller;
+        double speed;
+        double sum, count, lastAve;
+        std::string filename;
 
 };
 
